a4257bb4c4016961b1f9a54160ac1476a70de8fb
Artur Cortez Souza da Silva <20221011110013@ifrn.local>
2024-08-12 12:03:00 -0300

    Inicio projeto django

--- jambo/lojaElibros/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LojaelibrosConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lojaElibros'
--- jambo/lojaElibros/models/cliente.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-
-class Cliente(AbstractUser):
-    teste = AbstractUser.username
