d3a88dff1c599759da530a7ce4cc9e981b0066d8
Artur-Cortez <souza.cortez.013@gmail.com>
2024-08-26 08:29:19 -0300

    Criação classe LivroPedido

--- eLibros/elibrosLoja/models/livropedido.py
+++ eLibros/elibrosLoja/models/livropedido.py
@@ -1,5 +1,13 @@
 from django.db import models
+from models import Pedido, Livro
+
+from validators import *
 
 from django.apps import apps
-
 Cliente = apps.get_model('accounts', 'Cliente')
+
+
+class LivroPedido(models.Model):
+    valor = models.DecimalField(max_digits=5, decimal_places=2, validators=[nao_negativo, nao_nulo])
+    pedido = models.ForeignKey(Pedido, related_name="Pedido_container", on_delete=models.SET_NULL)
+    livro = models.ForeignKey(Livro, related_name="livro_de_um_pedido", on_delete=models.SET_NULL)
