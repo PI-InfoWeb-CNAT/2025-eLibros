6a157e05e203dbdbfc5c262efa9a497a1b8d761a
Artur-Cortez <souza.cortez.013@gmail.com>
2024-08-26 08:03:25 -0300

    Registro dos modelos no admin do django

--- eLibros/elibrosLoja/admin.py
+++ eLibros/elibrosLoja/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from models import *
+
+from django.apps import apps
+Cliente = apps.get_model('accounts', 'Cliente')
+
+admin.register(Endereco, Livro, LivroPedido, Pedido, Cliente)
