ff3a54cc14d566399073a4cfbd277df0258a8fb4
Artur-Cortez <souza.cortez.013@gmail.com>
2024-08-26 08:28:54 -0300

    Registro de modelos

---
+++ eLibros/elibrosLoja/models/__init__.py
@@ -0,0 +1,7 @@
+import imp
+from endereco import Endereco
+from livro import Livro
+from livropedido import LivroPedido
+from pedido import Pedido
+from generoTextual import GeneroTextual
+from generoLivro import GeneroLivro
