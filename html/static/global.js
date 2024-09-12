64ccce569407ed87e753638ef8bfd0a56045c1f4
RAFAEL RICCO DANTAS CAVALCANTI <20221011110007@ifrn.local>
2024-09-12 15:52:43 -0300

    carrinho

--- html/static/global.js
+++ html/static/global.js
@@ -1,19 +1,28 @@
 function increase() {
-    let input = document.getElementById('quantity');
-    let value = parseInt(input.value);
-    let max = parseInt(input.getAttribute('max')); // Converte para número
-  
-    if (value < max) {
-      input.value = value + 1;
-    }
+  let input = document.getElementById('quantity');
+  let value = parseInt(input.value);
+  let max = parseInt(input.getAttribute('max')); // Converte para número
+
+  if (value < max) {
+    input.value = value + 1;
   }
-  
+}
+
 function decrease() {
-    let input = document.getElementById('quantity');
-    let value = parseInt(input.value);
-    let min = parseInt(input.getAttribute('min')); // Converte para número
-  
-    if (value > min) {
-      input.value = value - 1;
-    }
-  }  
+  let input = document.getElementById('quantity');
+  let value = parseInt(input.value);
+  let min = parseInt(input.getAttribute('min')); // Converte para número
+
+  if (value > min) {
+    input.value = value - 1;
+  }
+}
+
+const selectAllCheckbox = document.getElementById('selectAll');
+const itemCheckboxes = document.querySelectorAll('.itemCheckbox');
+
+selectAllCheckbox.addEventListener('change', () => {
+  itemCheckboxes.forEach(checkbox => {
+    checkbox.checked = selectAllCheckbox.checked;
+  });
+});
