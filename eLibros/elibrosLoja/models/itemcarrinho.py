1d979c722158b7aa68ad3fa205af3c5a7b0516d9
Poti-team <ifrn.potiguaras@gmail.com>
2024-09-15 22:15:30 -0300

    Mudança de preço para preco

--- eLibros/elibrosLoja/models/itemcarrinho.py
+++ eLibros/elibrosLoja/models/itemcarrinho.py
@@ -5,12 +5,12 @@ from elibrosLoja.models.livro import Livro
 class ItemCarrinho(models.Model):
     livro = models.ForeignKey(Livro, null=False, related_name="livro_selecionado", on_delete=models.CASCADE)
     quantidade = models.IntegerField()
-    preço = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
+    preco = models.DecimalField(max_digits=5, decimal_places=2, blank=True)
 
     def __str__(self):
         return f"{self.quantidade}x {self.livro.titulo}"
     
     def save(self, *args, **kwargs):
-        if not self.preço:
-            self.preço = self.livro.preco
+        if not self.preco:
+            self.preco = self.livro.preco
         super().save(*args, **kwargs)
