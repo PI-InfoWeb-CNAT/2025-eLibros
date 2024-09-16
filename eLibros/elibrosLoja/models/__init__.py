f2a35978787dc1404f7b89d1e1f73f04fc128e16
Poti-team <ifrn.potiguaras@gmail.com>
2024-09-15 22:10:52 -0300

    Adição das entidades Gênero Literário e Categoria

--- eLibros/elibrosLoja/models/__init__.py
+++ eLibros/elibrosLoja/models/__init__.py
@@ -2,6 +2,7 @@ from .endereco import Endereco
 from .livro import Livro
 from .pedido import Pedido
 from .generoTextual import GeneroTextual
-from .generoLivro import GeneroLivro
+from .generoLiterario import GeneroLiterario
 from .carrinho import Carrinho
 from .itemcarrinho import ItemCarrinho
+from .categoria import Categoria
