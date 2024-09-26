6ba3af088e70cdf78f79bf23e6f0cd27f2ce9317
Artur-Cortez <souza.cortez.013@gmail.com>
2024-09-26 14:42:03 -0300

    adição de url de remover item do carrinho

--- eLibros/elibrosLoja/urls.py
+++ eLibros/elibrosLoja/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import AboutPageView, acervo, Inicio, livro, comprar_agora, perfil, ver_carrinho, admin
+from .views import AboutPageView, acervo, Inicio, livro, comprar_agora, perfil, ver_carrinho, admin, remover_itemcarrinho
 
 urlpatterns = [
     path("", Inicio, name="inicio"),
@@ -10,6 +10,9 @@ urlpatterns = [
     path("carrinho/", ver_carrinho, name="carrinho"),
     
     path("carrinho/<str:titulo>", comprar_agora, name="comprar_agora"),
+
+    path("carrinho/remover/<int:id>", remover_itemcarrinho, name="remover_itemcarrinho"),
+
     path("perfil/", perfil, name="perfil"),
 
     path("admin/", admin, name="admin"),
