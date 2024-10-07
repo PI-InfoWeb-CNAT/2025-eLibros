9f0ba008136bc928b5f5444e19930f8b729c18b9
Artur-Cortez <souza.cortez.013@gmail.com>
2024-10-07 12:12:58 -0300

    remoção urls desnecessários

--- eLibros/elibrosLoja/urls.py
+++ eLibros/elibrosLoja/urls.py
@@ -1,17 +1,15 @@
 from django.urls import path
-
-from .views import AboutPageView, acervo, Inicio, livro, comprar_agora, perfil, ver_carrinho, admin, remover_itemcarrinho, adicionar_itemcarrinho, atualizar_quantidade
+from .views import *
 
 urlpatterns = [
     path("", Inicio, name="inicio"),
     path("about/", AboutPageView.as_view(), name="about"),
     path("acervo/", acervo, name="acervo"),
     path("acervo/livro/<int:id>", livro, name="livro"),
+
+    path("atualizar_carrinho", atualizar_carrinho, name="atualizar_carrinho"),
     path("carrinho/", ver_carrinho, name="carrinho"),
-    path("carrinho/<int:id>", comprar_agora, name="comprar_agora"),
-    path("carrinho/adicionar/<int:id>", adicionar_itemcarrinho, name="adicionar_itemcarrinho"),
-    path("carrinho/atualizar/<int:id>", atualizar_quantidade, name="atualizar_quantidade"),
-    path("carrinho/remover/<int:id>", remover_itemcarrinho, name="remover_itemcarrinho"),
+    
     path("perfil/", perfil, name="perfil"),
     path("admin/", admin, name="admin"),
 ]
