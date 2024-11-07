63a419713d8dabe89d4eaca8b9e28f301f52bd20
Artur Cortez <souza.cortez.013@gmail.com>
2024-11-07 10:29:26 -0300

    Update settings.py

--- eLibros/elibrosAdmin/settings.py
+++ eLibros/elibrosAdmin/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     "default": {
         'ENGINE': 'mssql',
         'NAME': 'eLibrosDB',
-        'USER': 'AdminElibros'DB',
+        'USER': 'AdminElibrosDB',
         'PASSWORD': 'gatineosFofineos',
         'HOST': 'db',
         'PORT': '1433',
