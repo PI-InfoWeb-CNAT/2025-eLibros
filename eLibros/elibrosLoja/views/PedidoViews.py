22d71a81af4213f57a91c03b95e8e7c4f7b8275e
Artur Cortez <souza.cortez.013@gmail.com>
2024-11-25 01:52:13 +0000

    desconto view html foda sla

--- eLibros/elibrosLoja/views/PedidoViews.py
+++ eLibros/elibrosLoja/views/PedidoViews.py
@@ -1,14 +1,20 @@
 from django.http import JsonResponse
+from eLibros.utils.context_processors import carrinho
 from elibrosLoja.models import Carrinho, Pedido, Desconto
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def aplicar_cupom(request):
     if request.method == 'POST':
         codigo_cupom = request.POST.get('codigo_cupom')
         try:
-            desconto = Desconto.objects.get(codigo=codigo_cupom, ativo=True)
-            valor_total_com_desconto = request.session.get('valor_total') * desconto.valor
-            return JsonResponse({'valor_total': valor_total_com_desconto})
+            desconto = Desconto.objects.get(codigo=codigo_cupom, ativo=True)            
+            subtotal_com_desconto = request.session.get('subtotal', 0) * desconto.valor
+            valor_desconto = request.session.get('subtotal', 0) - subtotal_com_desconto
+            valor_total = subtotal_com_desconto + request.session.get('frete', 0)
+
+            request.session['desconto'] = valor_desconto
+            request.session['valor_total'] = valor_total
+            return JsonResponse({'valor_desconto': valor_desconto, 'valor_total': valor_total})
 
         except Desconto.DoesNotExist:
             return JsonResponse({'error': 'Cupom inválido ou expirado'}, status=400)
@@ -23,20 +29,22 @@ def finalizar_compra(request):
 
             subtotal = sum([item.produto.preco * item.quantia_itens for item in items])
             quantia_itens = sum([item.quantidade for item in items])
+            
+            frete = 12.99
+            request.session['frete'] = frete
+            request.session['subtotal'] = subtotal
 
-            frete = 12,99
+            if 'desconto' in request.session:
+                desconto = request.session['desconto']
+            else: desconto = 0
 
-            valor_total = subtotal + frete
-            request.session['valor_total'] = valor_total
-
-            context = {'frete': frete, 'subtotal': subtotal, 'quantia_itens': quantia_itens, 'items': items}
-
+            valor_total = (subtotal - desconto) + frete
+            context = {'frete': frete, 'subtotal': subtotal, 'quantia_itens': quantia_itens, 'items': items, 'desconto': desconto, 'valor_total': valor_total}
             return render(request, 'elibrosLoja/finalizar_compra.html', context=context)
        
     else:
         #redirecionar para página de login
-        session_id = request.session.get('session_id')
-        carrinho = Carrinho.objects.filter(session_id=session_id).first()
+        return redirect('login')
 
 def finalizar_compra_postback(request):
    # cria entidade pedido para armazenar no banco de dados
