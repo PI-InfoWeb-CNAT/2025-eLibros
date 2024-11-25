cb22908bda622d799327c5be8fbdf5b50ac190b7
Artur Cortez <souza.cortez.013@gmail.com>
2024-11-25 00:31:14 +0000

    Url da view que vai aplicar o cupom sobre o pedido

--- eLibros/elibrosLoja/urls/PedidoUrls.py
+++ eLibros/elibrosLoja/urls/PedidoUrls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from elibrosLoja.views import finalizar_compra
+from elibrosLoja.views import finalizar_compra, aplicar_cupom
 
 urlpatterns = [
+    path("aplicar_cupom", aplicar_cupom, name="aplicar_cupom"),
     path("finalizar_compra", finalizar_compra, name="finalizar_compra"),
+    
 ]
