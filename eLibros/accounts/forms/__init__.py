9af70374fc0388def9ba8d583b61987146e76b76
Artur Cortez <souza.cortez.013@gmail.com>
2025-01-20 19:58:12 +0000

    Renamed AuthForms

--- eLibros/accounts/forms/__init__.py
+++ eLibros/accounts/forms/__init__.py
@@ -1,5 +1,5 @@
 from .CustomUserChangeForm import CustomUserChangeForm
 from .CustomUserCreationForm import CustomUserCreationForm
 
-from .CustomLoginForm import CustomLoginForm
-from .CustomSignupForm import CustomSignupForm
+from .CustomLoginForm import LoginForm
+from .CustomSignupForm import SignupForm
