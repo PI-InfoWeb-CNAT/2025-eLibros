a1d74bf80dc89651f86d5457a02693a4f76c1048
Artur Cortez <souza.cortez.013@gmail.com>
2025-01-31 12:24:40 +0000

    remoção debug toolbar

--- eLibros/elibrosAdmin/urls.py
+++ eLibros/elibrosAdmin/urls.py
@@ -8,19 +8,14 @@ from django.urls import path, include
 urlpatterns = [
     path("djangoadmin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path('verification/', include('verify_email.urls')),
     path("", include("elibrosLoja.urls.HomeUrls")),
     path("acervo/", include("elibrosLoja.urls.LivroUrls")),
     path("cliente/", include("elibrosLoja.urls.ClienteUrls")),
     path("carrinho/", include("elibrosLoja.urls.CarrinhoUrls")),
-    path("admin/", include("elibrosLoja.urls.AdminUrls")) 
+    path("admin/", include("elibrosLoja.urls.AdminUrls")),
+    path("pedido/", include("elibrosLoja.urls.PedidoUrls")),
 ]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns = [
-        path("__debug__/", include(debug_toolbar.urls)),
-    ] + urlpatterns
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
