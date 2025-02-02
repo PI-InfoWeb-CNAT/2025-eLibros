628a127213ea54da0ba4938de57f8acadf4f8481
Rafael Ricco <124226801+rafaeltod@users.noreply.github.com>
2025-02-02 03:56:36 +0000

    modal cancelar pedido e confirmar pedido

--- eLibros/static/js/modal.js
+++ eLibros/static/js/modal.js
@@ -1,29 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var confirmButtons = document.querySelectorAll('.confirmar');
-    var closeButtons = document.querySelectorAll('.close');
+// Get the modal
+var modal = document.getElementById("modal_confirmar");
 
-    console.log(confirmButtons);
-    console.log(closeButtons);
+// Get the button that opens the modal
+var btn = document.getElementById("confirmar");
 
-    confirmButtons.forEach(function(btn) {
-        btn.addEventListener('click', function() {
-            var pedidoId = btn.getAttribute('data-pedido-id');
-            var modal = document.getElementById('modal_confirmar_' + pedidoId);
-            modal.style.display = "block";
-        });
-    });
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-    closeButtons.forEach(function(span) {
-        span.addEventListener('click', function() {
-            var pedidoId = span.getAttribute('data-pedido-id');
-            var modal = document.getElementById('modal_confirmar_' + pedidoId);
-            modal.style.display = "none";
-        });
-    });
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
-    window.addEventListener('click', function(event) {
-        if (event.target.classList.contains('modal_confirmar')) {
-            event.target.style.display = "none";
-        }
-    });
-});
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
