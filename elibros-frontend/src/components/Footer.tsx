96a85a0791c6312a42fc90490269abfebfe92a08
caua2512 <caua.h@escolar.ifrn.edu.br>
2025-07-17 17:32:40 -0300

    Footer Finalizado

--- elibros-frontend/src/components/Footer.tsx
+++ elibros-frontend/src/components/Footer.tsx
@@ -3,19 +3,26 @@ interface FooterProps {}
 export default function Footer({}: FooterProps) {
   return (
     <footer className="mt-20 px-4 md:px-20 py-8 bg-[#1C1607]">
-      <div className="flex flex">
+      <div className="flex flex-row justify-between pb-6.5 border-2 border-b-stone-800">
         <h1>
           <a href="/" className="flex">
             <img src="/logo.png" alt="eLibros" className="w-48 md:w-52" />
           </a>
         </h1>
-        <div className="flex flex-row gap-4">
-          <img src="/IN.png" alt="IN" className="" />
-          <img src="/Facebook.png" alt="Facebook" className="" />
-          <img src="/Twitter.png" alt="Twitter" className="" />
-          <img src="/instagram.png" alt="Instagram" className="" />
+        <div className="flex flex-row gap-4 items-center">
+          <div className="border-stone-800 border-2 w-15 h-15 rounded-full flex justify-center items-center"><img width="14px" height="14px" src="/IN.png" alt="IN" /></div>
+          <div className="border-stone-800 border-2 w-15 h-15 rounded-full flex justify-center items-center"><img width="11px" height="11px" src="/Facebook.png"  alt="Facebook" /></div>
+          <div className="border-stone-800 border-2 w-15 h-15 rounded-full flex justify-center items-center"><img width="18px" height="18px" src="/Twitter.png"  alt="Twitter" /></div>
+          <div className="border-stone-800 border-2 w-15 h-15 rounded-full flex justify-center items-center"><img width="18px" height="18px" src="/instagram.png"  alt="Instagram" /></div>
         </div>
-      <p className="text-white text-center">@ 2024 Entregadores. Todos os direitos reservados</p>
+    </div>
+    <div className="mt-6 flex flex-row justify-between ">
+      <p className="text-white text-left">@ 2024 Entregadores. Todos os direitos reservados</p>
+      <div className="flex flex-row text-right gap-9">
+        <p className="text-white">Termos</p>
+        <p className="text-white">Privacidade</p>
+        <p className="text-white">Cookies</p>
+      </div>
     </div>
     </footer>
   );
