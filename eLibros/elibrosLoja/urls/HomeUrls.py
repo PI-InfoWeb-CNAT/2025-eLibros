a71d059dd68159d80f8489b5a91c78fa2e0c83d3
Olga Cortez <souza.cortez.013@gmail.com>
2025-07-30 18:19:30 +0000

    remoção urls monolíticas

--- eLibros/elibrosLoja/urls/HomeUrls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from elibrosLoja.views import LivroViews
-
-urlpatterns = [
-    path("", LivroViews.Inicio, name="inicio"),
-
-]
--- eLibros/elibrosLoja/urls/LivroUrls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from elibrosLoja.views import LivroViews
-
-urlpatterns = [
-    path("", LivroViews.acervo, name="acervo"),
-    path("livro/<int:id>", LivroViews.livro, name="livro"),
-    path("explorar/", LivroViews.explorar, name="explorar"),
-    path("explorar/<str:busca>", LivroViews.explorar, name="explorar"),
-]
