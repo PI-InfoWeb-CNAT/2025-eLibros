17e437415ab5a9c962b398bf8716bd1effac777f
Olga Cortez <souza.cortez.013@gmail.com>
2025-10-30 00:34:29 +0000

    Correção settings.py na conexão com a api

--- elibrosAdmin/settings.py
+++ elibrosAdmin/settings.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import os
 import dj_database_url
 
+from dotenv import load_dotenv
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
@@ -122,31 +125,15 @@ TEMPLATES = [
 # Configuração do banco de dados Neon/PostgreSQL
 
 
-NEON_DB_URL = os.getenv("NEON_DB_PROD") or os.getenv("NEON_DB_DEV")
+NEON_DB_URL = os.getenv("NEON_DB_DEV")
 if NEON_DB_URL:
     DATABASES = {
-        "default": dj_database_url.parse(NEON_DB_URL, conn_max_age=600, ssl_require=True)
-    }
-else:
-    # Fallback para sqlite se não houver variável de ambiente
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": "eLibrosDB",
-        }
-    }
-
-# DATABASES = {
-#     'default': {
-#         "ENGINE": "django.db.backends.postgresql",
-#         'NAME': "eLibrosDB",
-#         'USER': "postgres",
-#         'PASSWORD': "gatineosFofineos",
-#         'HOST': 'db',
-#         'PORT': 5432,
-#     }
-# }
-
+        "default": dj_database_url.config(
+            default=NEON_DB_URL,
+            conn_max_age=600,
+            ssl_require=True
+            )
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
